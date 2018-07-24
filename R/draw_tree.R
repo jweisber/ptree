f5a9b2af5f1a398677c9f9785f3ccad81fb024a2
Jonathan Weisberg <jonathan.weisberg@utoronto.ca>
2018-07-24 11:11:09 -0400

    add padding option to draw_tree()

--- R/draw_tree.R
+++ R/draw_tree.R
@@ -7,11 +7,16 @@
 #' @examples
 #' draw_tree(nodes)
 
-draw_tree <- function(nodes) {
+draw_tree <- function(nodes, padding = unit(.75, "lines")) {
+
+  if (class(padding) == "numeric") {
+    padding <- unit(padding, "lines")
+  }
+
   p <- ggplot(nodes) +
     theme_void() + xlim(0, 1) + ylim(0, 1) +
     geom_segment(aes(x = x, y = y, xend = xend, yend = yend)) +
-    geom_label(aes(x = x, y = y, label = text), label.size = NA)
+    geom_label(aes(x = x, y = y, label = text), label.size = NA, label.padding = padding)
 
   if ("p" %in% colnames(nodes)) {
     p <- p + geom_text(aes(x = p_x, y = p_y, label = p, angle = p_angle))
